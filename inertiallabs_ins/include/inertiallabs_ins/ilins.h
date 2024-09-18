657ea1590b52bd81003c7fd901d0f01774d22c34
youwei.cao <gofar@outlook.com>
2024-09-18 20:25:12 +0800

    add imu_raw publisher

--- inertiallabs_ins/include/inertiallabs_ins/ilins.h
+++ inertiallabs_ins/include/inertiallabs_ins/ilins.h
@@ -5,6 +5,7 @@
 #include "inertiallabs_msgs/msg/gps_data.hpp"
 #include "inertiallabs_msgs/msg/gnss_data.hpp"
 #include "inertiallabs_msgs/msg/marine_data.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 class il_ins : public rclcpp::Node
 {
 private:
@@ -18,4 +19,5 @@ public:
     rclcpp::Publisher<inertiallabs_msgs::msg::GpsData>::SharedPtr publishers2;
     rclcpp::Publisher<inertiallabs_msgs::msg::GnssData>::SharedPtr publishers3;
     rclcpp::Publisher<inertiallabs_msgs::msg::MarineData>::SharedPtr publishers4;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr publishers5;
 };
